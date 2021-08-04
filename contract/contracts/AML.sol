63fd3bbf94f53bd6c779cc725652007ec06d3ef9
MinuraPunchihewa <minurapunchihewa17@gmail.com>
2021-08-04 10:54:25 +0530

    added multiple admins

--- contract/contracts/AML.sol
+++ contract/contracts/AML.sol
@@ -1,4 +1,4 @@
-pragma solidity =0.8.1;
+pragma solidity >=0.4.18;
 
 contract AML{
     struct Org{
@@ -9,26 +9,37 @@ contract AML{
         string RegisteredAddress;
         string isDomiciledIn;
         bool sanStatus;
-        address admin;
+        mapping(address => bool) admins;
     }
     
     mapping(address => Org) public orgs;
-    
-    mapping(address => address[]) public views;
     
     address public sender;
     address payable public receiver;
     
     function addOrg(address org, uint _international_securities_identifier, string calldata _organization_name, bool _hasActivityStatus, string calldata _businessClassifier, string calldata _RegisteredAddress, string calldata _isDomiciledIn, bool _sanStatus) external{
-        orgs[org] = Org(_international_securities_identifier, _organization_name, _hasActivityStatus, _businessClassifier, _RegisteredAddress, _isDomiciledIn, _sanStatus, msg.sender);
+        Org storage organization = orgs[org];
+        organization.international_securities_identifier = _international_securities_identifier;
+        organization.organization_name = _organization_name;
+        organization.hasActivityStatus = _hasActivityStatus;
+        organization.businessClassifier = _businessClassifier;
+        organization.RegisteredAddress = _RegisteredAddress;
+        organization.isDomiciledIn = _isDomiciledIn;
+        organization.sanStatus = _sanStatus;
+        organization.admins[msg.sender] = true;
+        // orgs[org] = Org(_international_securities_identifier, _organization_name, _hasActivityStatus, _businessClassifier, _RegisteredAddress, _isDomiciledIn, _sanStatus, {msg.sender: true});
     }
     
     function showOrg(address org) external view returns(uint international_securities_identifier, string memory organization_name, bool hasActivityStatus, string memory businessClassifier, string memory RegisteredAddress, string memory isDomiciledIn, bool sanStatus){
         return(orgs[org].international_securities_identifier, orgs[org].organization_name, orgs[org].hasActivityStatus, orgs[org].businessClassifier, orgs[org].RegisteredAddress, orgs[org].isDomiciledIn, orgs[org].sanStatus);
     }
+
+    function addAdmin(address org, address admin) external{
+        orgs[org].admins[admin] = true;
+    }
     
     function updateOrg(address org, uint _international_securities_identifier, string calldata _organization_name, bool _hasActivityStatus, string calldata _businessClassifier, string calldata _RegisteredAddress, string calldata _isDomiciledIn, bool _sanStatus) external{
-        require(msg.sender == orgs[org].admin, "You are not authorized to make this change!");
+        require(orgs[org].admins[msg.sender] = true, "You are not authorized to make this change!");
         orgs[org].international_securities_identifier = _international_securities_identifier;
         orgs[org].organization_name = _organization_name;
         orgs[org].hasActivityStatus = _hasActivityStatus;
